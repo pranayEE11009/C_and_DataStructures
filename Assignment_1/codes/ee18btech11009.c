08e7df890a0537ca63003d8b4628db0e5447555d
pranayEE11009 <47570453+pranayEE11009@users.noreply.github.com>
2021-10-11 19:43:12 +0530

    Add files via upload

---
+++ Assignment_1/codes/ee18btech11009.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int SimpleFunction(int Y[], int n, int x){
+    int total = Y[0], loopIndex;
+    for( loopIndex = 1; loopIndex<=n-1; loopIndex++){
+        total = x*total + Y[loopIndex];
+        printf("for loopIndex=%d - total : %d\n",loopIndex, total);
+    }
+    return total;
+}
+int main()
+{
+    int Z[10] = {1,1,1,1,1,1,1,1,1,1};
+    printf("Final solution of the SimpleFunction for n = 10 is: %d", SimpleFunction(Z, 10, 2));
+
+    return 0;
+}
